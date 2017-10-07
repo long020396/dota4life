e236e135854da5dd272d879f74b01b8c3ad85554
LONGDDSE62137\Du <long020396@gmail.com>
2017-10-07 19:49:26 +0700

    Turn IDENTITY_INSERT on
    - Add tag @GeneratedValue(strategy=GenerationType.IDENTITY) for entity classes

--- Dota4Life/src/java/entities/Hero.java
+++ Dota4Life/src/java/entities/Hero.java
@@ -12,6 +12,8 @@ import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -38,6 +40,7 @@ import javax.xml.bind.annotation.XmlTransient;
 public class Hero implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "ID")
     private Integer id;
